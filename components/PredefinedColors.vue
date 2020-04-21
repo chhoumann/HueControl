32283df9f5c0aae73bf83cc9d194d8e82edc0b7a
Christian Bager Bach Houmann <chhoumann@gmail.com>
2020-04-21 22:43:57 +0200

    Predefined color changer in progress

---
+++ components/PredefinedColors.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="columns">
+      <v-container>
+        <v-card v-for="color in colors" :key="color">
+          <h1 align="center">{{ color }}</h1>
+        </v-card>
+      </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PredefinedColors',
+  props: ["light"],
+  data() {
+    return {
+      colors: ["White", "Red", "Blue"]
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: decimal(65535)
+}
+</style>
